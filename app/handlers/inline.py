0f9d3f2899505109fc8e902e7033cf5c7c4c2918
AcruxTech <acrux_tech@mail.ru>
2023-06-23 01:04:37 +0300

    add fecne font and shrug emoji inline queries

--- app/handlers/inline.py
+++ app/handlers/inline.py
@@ -2,43 +2,49 @@ from aiogram import Dispatcher, types
 
 from app.services.flow import Flow
 from app.services.palindrome import Palindrome
+from app.services.misc_functions import to_fence_font
 
 
 async def inline_handler(query: types.InlineQuery):
     p = Palindrome()
     f = Flow()
-    response = [
-        types.InlineQueryResultArticle(
-            id=0,
-            title='Палиндромы',
-            input_message_content=types.InputTextMessageContent(
-                message_text=f'Ближайший полин гном: <b>{p.get_next().strftime("%H:%M")}</b>\nДо него осталось <b>{p.get_time_to_next()}</b>',
-                parse_mode='HTML'
-            )
-        ),
-        types.InlineQueryResultArticle(
-            id=1,
-            title='Потоки',
-            input_message_content=types.InputTextMessageContent(
-                message_text=f'Ближайший поток: <b>{f.get_next().strftime("%H:%M")}</b>\nДо него осталось <b>{f.get_time_to_next()}</b>',
-                parse_mode='HTML'
-            )
-        ),
-        types.InlineQueryResultArticle(
-            id=2,
-            title='Шрифт заборчиком',
-            input_message_content=types.InputTextMessageContent(
-                message_text='потом сделаю',
-            )
-        ),
-        types.InlineQueryResultArticle(
-            id=3,
-            title='Shrug эмодзи',
-            input_message_content=types.InputTextMessageContent(
-                message_text='пока лень делать',
-            )
-        ),
-    ]
+    if query.query:
+        response = [
+            # БаЛьШиЕ и МаЛеНьКиЕ БуКаФфКи ДаЛжНЫ ЧерЕдАвАцЦо, ЧмЯФфК
+            types.InlineQueryResultArticle(
+                id=0,
+                title='Шрифт заборчиком',
+                input_message_content=types.InputTextMessageContent(
+                    message_text=to_fence_font(query.query),
+                )
+            ),
+            types.InlineQueryResultArticle(
+                id=1,
+                title='Shrug эмодзи',
+                input_message_content=types.InputTextMessageContent(
+                    message_text=f'{query.query} ¯\_(ツ)_/¯',
+                )
+            ),
+        ]
+    else:
+        response = [
+            types.InlineQueryResultArticle(
+                id=0,
+                title='Палиндромы',
+                input_message_content=types.InputTextMessageContent(
+                    message_text=f'Ближайший полин гном: <b>{p.get_next().strftime("%H:%M")}</b>\nДо него осталось <b>{p.get_time_to_next()}</b>',
+                    parse_mode='HTML'
+                )
+            ),
+            types.InlineQueryResultArticle(
+                id=1,
+                title='Потоки',
+                input_message_content=types.InputTextMessageContent(
+                    message_text=f'Ближайший поток: <b>{f.get_next().strftime("%H:%M")}</b>\nДо него осталось <b>{f.get_time_to_next()}</b>',
+                    parse_mode='HTML'
+                )
+            ),
+        ]
     await query.answer(response, is_personal=True, cache_time=10)
 
 
--- app/services/misc_functions.py
+++ app/services/misc_functions.py
@@ -1,2 +1,13 @@
 def get_reversed_number(number: int) -> int:
     return int(str(number).rjust(2, '0')[::-1])
+
+
+def to_fence_font(seq: str) -> str:
+    seq = list(seq.lower())
+    for i, symbol in enumerate(seq):
+        if not symbol.isalpha():
+            continue
+        if sum(map(str.isalpha, seq[:i])) % 2 == 0:
+            seq[i] = symbol.upper()
+        
+    return ''.join(seq)
